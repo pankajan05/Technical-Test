7206c8e081c54126d6f70d52a8ebc0a4c6e614af
Pankajan Satkunam <pankajan05@gmail.com>
2024-04-28 13:55:16 +0530

    create the overall UI

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,16 @@
 import { Routes } from '@angular/router';
 import { PlantListComponent } from './plant-list/plant-list.component';
+import { PlantDetailComponent } from './plant-detail/plant-detail.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: PlantListComponent,
     title: 'Home page',
+  },
+  {
+    path: 'plants/:id',
+    component: PlantDetailComponent,
+    title: 'Plant details',
   },
 ];
--- src/app/plant-detail/plant-detail.component.ts
+++ src/app/plant-detail/plant-detail.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { PlantDescriptionComponent } from './plant-description/plant-description.component';
+import { PlantContactComponent } from './plant-contact/plant-contact.component';
 
 @Component({
   selector: 'app-plant-detail',
   standalone: true,
-  imports: [],
+  imports: [PlantDescriptionComponent, PlantContactComponent],
   templateUrl: './plant-detail.component.html',
-  styleUrl: './plant-detail.component.css'
+  styleUrl: './plant-detail.component.css',
 })
-export class PlantDetailComponent {
-
-}
+export class PlantDetailComponent {}
